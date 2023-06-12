5ada366db36b78047083b6c1ce0a4b27aac77ce5
Mark Mansfield <mark.mansfield@barrelproof.net>
2023-06-12 19:18:20 +0100

    Add under development label

--- app/src/main/java/com/squizzard/about/AboutActivity.kt
+++ app/src/main/java/com/squizzard/about/AboutActivity.kt
@@ -1,54 +1,55 @@
-package com.squizzard.about;
+package com.squizzard.about
 
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
+import com.squizzard.analytics.AnalyticsHelper.sendEvent
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import com.squizzard.misriCalendar.R
+import android.os.Build
+import android.view.MenuItem
+import android.view.View
+import com.squizzard.misriCalendar.BuildConfig
 
-import com.squizzard.analytics.AnalyticsHelper;
-import com.squizzard.misriCalendar.R;
-import com.squizzard.util.Utility;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class AboutActivity extends AppCompatActivity implements OnClickListener{
-
-	final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-
-	protected void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
-		if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+class AboutActivity : AppCompatActivity(), View.OnClickListener {
+    private val emailIntent = Intent(Intent.ACTION_SEND)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about)
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         }
-		findViewById(R.id.emailButton).setOnClickListener(this);
-	}
+        findViewById<View>(R.id.emailButton).setOnClickListener(this)
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem menuItem) {
-	    onBackPressed();
-	    return true;
-	}
+    override fun onOptionsItemSelected(menuItem: MenuItem): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
+    }
 
-	public void onClick(View v) {
-		if (v.getId() == R.id.emailButton){
-			AnalyticsHelper.sendEvent("contact_developer");
-
-			String messageString = "\n\n\n\n OS Version: " + System.getProperty("os.version");
-			messageString += "\n OS API Level: " + Build.VERSION.RELEASE;
-			messageString += "\n Device: " + android.os.Build.DEVICE;
-			messageString += "\n Model : " + android.os.Build.MODEL;
-			messageString += "\n Display: " + android.os.Build.DISPLAY;
-			messageString += "\n Manufacturer: " + android.os.Build.MANUFACTURER;
-			emailIntent.setType("plain/text");
-			emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{getString(R.string.ouikka_email)});
-			emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-                    "MisriCal "+ Utility.getVersion(getApplicationContext()));
-			emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, messageString);
-			emailIntent.setType("message/rfc822");
-			startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-		}	
-	}
+    override fun onClick(v: View) {
+        if (v.id == R.id.emailButton) {
+            sendEvent("contact_developer")
+            var messageString = """
+            OS Version: ${System.getProperty("os.version")}"""
+            messageString += """
+            OS API Level: ${Build.VERSION.RELEASE}"""
+            messageString += """
+            Device: ${Build.DEVICE}"""
+            messageString += """
+            Model : ${Build.MODEL}"""
+            messageString += """
+            Display: ${Build.DISPLAY}"""
+            messageString += """
+            Manufacturer: ${Build.MANUFACTURER}"""
+            emailIntent.type = "plain/text"
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.ouikka_email)))
+            emailIntent.putExtra(
+                Intent.EXTRA_SUBJECT,
+                "MisriCal " + BuildConfig.VERSION_NAME
+            )
+            emailIntent.putExtra(Intent.EXTRA_TEXT, messageString)
+            emailIntent.type = "message/rfc822"
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."))
+        }
+    }
 }
